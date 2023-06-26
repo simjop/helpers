5f5ee6590a87ebf96d0f899ec2365e9c3b9d2667
Tibor Dudlák <tdudlak@redhat.com>
2023-06-26 15:56:52 +0200

    Add upgrade script with minimal packages installation for f38
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# helper-cripts
+# helper-scripts
 Helper scripts to manage server
+
+## Running scrip simply using curl
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/simjop/helper-scripts/main/upgrade-install.sh)"
+```
+
+## Running script simply using wget
+
+```
+sh -c "$(wget https://raw.githubusercontent.com/simjop/helper-scripts/main/upgrade-install.sh -O -)"
+```
---
+++ upgrade-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Upgrade to latest fedora (max 2 releases ahead so minimal version is 35)
+
+ls dnf-updated || echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+ls dnf-updated || echo "deltarpm=true" >> /etc/dnf/dnf.conf
+ls dnf-updated || echo "fastestmirror=true" >> /etc/dnf/dnf.conf
+ls dnf-updated || touch ~/dnf-updated
+
+dnf upgrade --refresh -y
+dnf install dnf-plugin-system-upgrade -y
+
+cat /etc/redhat-release | grep 35 && echo "Update to 37" && dnf system-upgrade download --releasever=37 -y
+cat /etc/redhat-release | grep 36 && echo "Update to 37" && dnf system-upgrade download --releasever=37 -y
+cat /etc/redhat-release | grep 37 && echo "Update to 38" && dnf system-upgrade download --releasever=38 -y
+
+dnf system-upgrade reboot
+
+cat /etc/redhat-release | grep 38 && echo "Install packages on 38" && dnf -y update && dnf install -y --allowerasing vim-enhanced vim-default-editor mc git tig htop iotop zsh
